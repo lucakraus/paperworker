7d1310b6000ae9c1a2004fbcc5606cc2c68178e2
cristobal fernandez <cristobal.sch@gmail.com>
2023-03-07 15:50:31 +0100

    add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Event.destroy_all
+User.destroy_all
+
+cristobal = User.create!(email: "cris.cris@gmail.com", password: "123456", first_name: "Cristobal", last_name: "Fernandez")
+luca = User.create!(email: "luca.luca@gmail.com", password: "123456", first_name: "Luca", last_name: "Kraus")
+
+paperworker = Event.create!(user_id: User.all.sample.id, email: "luca.luca@gmail.com",first_name: "Luca", last_name: "Kraus")
+file = URI.open("https://res.cloudinary.com/dxrk5d6qu/image/upload/v1678199508/paperw%C3%B6rker/profile%20pictures/Luca_dwzaly.jpg")
+paperworker.photo.attach(io: file, filename: "Luca.jpeg", content_type: "image/jpeg")
+paperworker.save
