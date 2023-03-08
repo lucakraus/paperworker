65b36393147d45190b47d368ff24af50ee10331c
Luca Kraus <lucakrausb@gmail.com>
2023-03-08 17:31:23 +0100

    seed and filter form changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,17 @@ class UsersController < ApplicationController
 
   def index
     @paperworkers = User.paperworker
-    # raise
     # @paperworkers = @paperworkers.where(languages: params[:languages]) if params[:languages].present?
     # @paperworkers = @paperworkers.where(district: params[:district]) if params[:district].present?
     # @paperworkers = @paperworkers.where(services: params[:services]) if params[:services].present?
-    @paperworkers = @paperworkers.select { |paperworker| paperworker.languages.include?(params[:languages]) } if params[:languages].present?
-    @paperworkers = @paperworkers.select { |paperworker| paperworker.district.include?(params[:district]) } if params[:district].present?
-    @paperworkers = @paperworkers.select { |paperworker| paperworker.services.include?(params[:services]) } if params[:services].present?
+    @paperworkers = @paperworkers.select { |paperworker| paperworker.languages.include?(params[:filter][:languages]) } if params[:filter][:languages].present?
+    @paperworkers = @paperworkers.select { |paperworker| paperworker.district.include?(params[:filter][:district]) } if params[:filter][:district].present?
+    @paperworkers = @paperworkers.select { |paperworker| paperworker.services.include?(params[:filter][:services]) } if params[:filter][:services].present?
   end
 
   def show
     @paperworker = User.paperworker.find(params[:id])
-    
+
   end
 
 end
