07f9919fc0bed675e1db593f3a4eaa466a42a96e
Akram Hassan <akramhassan1502@gmail.com>
2023-03-08 16:26:15 +0100

    Made the request function for an appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,23 @@
 class AppointmentsController < ApplicationController
+   def new
+    @appointment = Appointment.new
+   end
+
+  def create
+    @paperworker = User.paperworker.find(params[:user_id])
+    @appointment = Appointment.new(appointment_params)
+    @appointment.paperworker = @paperworker
+    @appointment.immigrant = current_user
+     if @appointment.save
+      redirect_to user_path(@paperworker)
+     else
+      render 'users/show', status: :unprocessable_entity
+     end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:date)
+  end
 end
