4c985d15e565b1aa8c71722d87a8f63b2701003c
Akram Hassan <akramhassan1502@gmail.com>
2023-03-08 12:59:37 +0100

    Showing info about paperworker

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
   def index
     @paperworkers = User.paperworker
   end
+
+  def show
+    @paperworker = User.paperworker.find(params[:id])
+    
+  end
+
 end
