291bb48005167f953b03a4fa9a9e7163913700e5
Akram Hassan <akramhassan1502@gmail.com>
2023-03-13 14:50:39 +0100

    fixed alert message

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("chat-subscription", ChatSubscriptionController)
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SweetAlertController from "./sweet_alert_controller"
+application.register("sweet-alert", SweetAlertController)
