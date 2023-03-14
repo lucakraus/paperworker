43cd9854e8663ebd2040f60385288d4c026c5150
BaharehSammaknejad <baharsammaknejad@gmail.com>
2023-03-14 12:46:46 +0100

    2 fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,4 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :languages, :services, :role])
   end
-
+end
